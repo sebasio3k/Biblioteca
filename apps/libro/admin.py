7e58c9bcca05ec840d9f11fc12b80e60afd7d9b4
SebastianHO <bastianhdezo@gmail.com>
2022-08-04 17:35:36 -0500

    django-import-export

--- apps/libro/admin.py
+++ apps/libro/admin.py
@@ -1,6 +1,49 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
 from .models import Autor, Libro
 
+
+class AutorResource(resources.ModelResource):
+    class Meta:
+        model = Autor
+
+
+class LibroResource(resources.ModelResource):
+    class Meta:
+        model = Libro
+
+
+class AutorAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    # Barra de busqueda:
+    search_fields = ['nombre', 'apellidos', 'nacionalidad']
+    # Para atributos en vista de registros:
+    list_display = [
+        'id',
+        'nombre',
+        'apellidos',
+        'nacionalidad',
+        'descripcion',
+        'estado',
+    ]
+    resource_class = AutorResource
+
+
+class LibroAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    # Barra de busqueda:
+    search_fields = ['titulo', 'fecha_publicacion']
+    # Para atributos en vista de registros:
+    list_display = [
+        'id',
+        'titulo',
+        'fecha_publicacion',
+        'fecha_creacion',
+        # 'estado',
+    ]
+    resource_class = AutorResource
+
+
 # Register your models here.
-admin.site.register(Autor)
-admin.site.register(Libro)
+admin.site.register(Autor, AutorAdmin)
+admin.site.register(Libro, LibroAdmin)
--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.libro',
+    'import_export',
+    'static',
 ]
 
 MIDDLEWARE = [
